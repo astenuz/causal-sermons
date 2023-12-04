8585fbebf6ca575021b58bbd680c90932c42e14c
David Gamba <ccontreras.gamba@gmail.com>
2023-12-04 18:15:35 -0500

    adding CATE

--- src/causal_sermons/ate.py
+++ src/causal_sermons/ate.py
@@ -5,22 +5,37 @@ def tau_naive(t, y):
     return y[t.flatten() == 1].mean(axis=0) - y[t.flatten() == 0].mean(axis=0)
 
 
-def tau_PI(q_t0, q_t1):
+def tau_PI_i(q_t0, q_t1):
     ite = (q_t1 - q_t0)
+    return ite
+
+
+def tau_PI(q_t0, q_t1):
+    ite = tau_PI_i(q_t0, q_t1)
     return ite.mean(axis=0)
 
 
 def tau_IPW(g, t, y):
-    ite = (t / g - (1 - t) / (1 - g)) * y
+    ite = tau_IPW_i(g, t, y)
     return ite.mean(axis=0)
+
+
+def tau_IPW_i(g, t, y):
+    ite = (t / g - (1 - t) / (1 - g)) * y
+    return ite
 
 
 def tau_DR(q_t0, q_t1, g, t, y):
+    ite = tau_DR_i(q_t0, q_t1, g, t, y)
+
+    return ite.mean(axis=0)
+
+
+def tau_DR_i(q_t0, q_t1, g, t, y):
     full_q = q_t0 * (1 - t) + q_t1 * t
     h = t * (1.0 / g) - (1.0 - t) / (1.0 - g)
     ite = h * (y - full_q) + q_t1 - q_t0
-
-    return ite.mean(axis=0)
+    return ite
 
 
 def all_ate_estimators(q_t0, q_t1, g, t, y):
